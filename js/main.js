fcc8ae4c5ce676d48c3cce2c351b8e6efb98f103
mfzr27 <mfzr27@gmail.com>
2017-11-01 09:43:51 -0200

    Material-Profile v1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var app = angular.module('mfzr27', ['ngRoute'])
+
+app.config(function($routeProvider, $locationProvider){
+
+	$routeProvider
+		.when('/home',{
+			templateUrl:'home.html'
+		})
+		.when('/projects',{
+			templateUrl:'projects.html'
+		})
+		.when('/blog',{
+			templateUrl:'blog.html'
+		})
+		.when('/contact',{
+			templateUrl:'contact.html'
+		})
+		.otherwise('/home');
+})
